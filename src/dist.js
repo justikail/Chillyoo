cf8a8e713f08c2955472d4c0d5985bb8f762538d
justikail <mininginformation1337@gmail.com>
2023-12-10 13:11:06 +0700

    initial commit

--- src/dist.js
+++ src/dist.js
@@ -12,3 +12,23 @@ document.addEventListener("click", function (e) {
     navibar.classList.remove("active");
   }
 });
+
+
+
+// javascript slider
+document.getElementById("next").onclick = function () {
+  const widthItem = document.querySelector(".card").offsetWidth;
+  document.getElementById("container").scrollLeft += widthItem;
+};
+document.getElementById("prev").onclick = function () {
+  const widthItem = document.querySelector(".card").offsetWidth;
+  document.getElementById("container").scrollLeft -= widthItem;
+};
+document.getElementById("next-2").onclick = function () {
+  const widthItem = document.querySelector(".card-2").offsetWidth;
+  document.getElementById("container-2").scrollLeft += widthItem;
+};
+document.getElementById("prev-2").onclick = function () {
+  const widthItem = document.querySelector(".card-2").offsetWidth;
+  document.getElementById("container-2").scrollLeft -= widthItem;
+};
